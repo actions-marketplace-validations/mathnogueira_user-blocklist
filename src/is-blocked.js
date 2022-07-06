101724b41218813fa04301870a276b78ddfc0f39
Matheus Nogueira <matheus.nogueira2008@gmail.com>
2022-07-06 18:41:24 -0300

    add debugging

--- src/is-blocked.js
+++ src/is-blocked.js
@@ -1,6 +1,7 @@
 const {context} = require('@actions/github');
 
 function isBlocked(core) {
+    core.debug('context', context);
     const {actor} = context || {};
     if (!actor) {
         core.setFailed(`Actor couldn't be extracted from event context`);
@@ -8,12 +9,14 @@ function isBlocked(core) {
     }
 
     const blockedUsers = core.getInput('blocked_users');
+    core.debug('blocked_users', blockedUsers);
     if (blockedUsers.trim().length === 0) {
         core.setFailed(`blocked_users was not specified`)
         return
     }
 
     const userIsBlocked = blockedUsers.split(',').map(user => user.trim()).some(user == actor);
+    core.debug('user is blocked', userIsBlocked);
     core.setOutput('result', userIsBlocked)
 }
 
