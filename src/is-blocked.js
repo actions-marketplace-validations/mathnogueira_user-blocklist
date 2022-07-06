ae007a5e2ae7617bdde2ac7b672c3ef502c095d6
Matheus Nogueira <matheus.nogueira2008@gmail.com>
2022-07-06 18:45:29 -0300

    format

--- src/is-blocked.js
+++ src/is-blocked.js
@@ -1,23 +1,26 @@
-const {context} = require('@actions/github');
+const { context } = require('@actions/github');
 
 function isBlocked(core) {
-    core.debug('context', context);
-    const {actor} = context || {};
-    if (!actor) {
-        core.setFailed(`Actor couldn't be extracted from event context`);
-        return
-    }
+  core.debug('context', context);
+  const { actor } = context || {};
+  if (!actor) {
+    core.setFailed(`Actor couldn't be extracted from event context`);
+    return;
+  }
 
-    const blockedUsers = core.getInput('blocked_users');
-    core.debug('blocked_users', blockedUsers);
-    if (blockedUsers.trim().length === 0) {
-        core.setFailed(`blocked_users was not specified`)
-        return
-    }
+  const blockedUsers = core.getInput('blocked_users');
+  core.debug('blocked_users', blockedUsers);
+  if (blockedUsers.trim().length === 0) {
+    core.setFailed(`blocked_users was not specified`);
+    return;
+  }
 
-    const userIsBlocked = blockedUsers.split(',').map(user => user.trim()).some(user == actor);
-    core.debug('user is blocked', userIsBlocked);
-    core.setOutput('result', userIsBlocked)
+  const userIsBlocked = blockedUsers
+    .split(',')
+    .map(user => user.trim())
+    .some(user == actor);
+  core.debug('user is blocked', userIsBlocked);
+  core.setOutput('result', userIsBlocked);
 }
 
 module.exports = isBlocked;
