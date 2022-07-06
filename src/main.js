b3f1d352a40960f3324a46fefeaad7b694c53a2b
Matheus Nogueira <matheus.nogueira2008@gmail.com>
2022-07-06 18:04:49 -0300

    add workflow to test it

--- src/main.js
+++ src/main.js
@@ -8,7 +8,7 @@ async function run() {
     const {actor} = context || {};
 
     const blockedUsersList = blockedUsers.split(',').map(user => user.trim());
-    const userIsBlocked = blockedUserList.contains(actor)
+    const userIsBlocked = blockedUsersList.contains(actor)
 
     core.setOutput('result', userIsBlocked ? "true" : "false");
   } catch (error) {
